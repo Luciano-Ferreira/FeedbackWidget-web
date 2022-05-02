67da8aae524a7bc85d0b2cd4b6976d2d796c6ba0
Luciano Ferreira <luciano2023silva@gmail.com>
2022-05-02 13:35:08 -0300

    vite cra with tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className='flex gap-2'>
+      <button className='button'>Hello world</button>
+      <button className='button'>Hello world</button>
+    </div>
+  )
+  
+}
+
+export default App
