b5f24e97206aade9b625da18450a326380915722
Luciano Ferreira <luciano2023silva@gmail.com>
2022-05-05 13:14:41 -0300

    screenshot logic and readme

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,4 +1,5 @@
 import { ArrowLeft } from 'phosphor-react';
+import { useState } from 'react';
 import { FeedbackType, feedbackTypes } from '..';
 import { CloseButton } from '../../CloseButton';
 import { ScreenShotButton } from '../ScreenShotButton';
@@ -12,6 +13,8 @@ export function FeedbackContentStep({
   feedbackType,
   onFeedbackRestartRequested
 }: FeedbackContentStepProps) {
+  const [screenshot, setScreenshot] = useState<string | null>(null)
+
   const feedbackTypeInfo = feedbackTypes[feedbackType]
   return (
     <>
