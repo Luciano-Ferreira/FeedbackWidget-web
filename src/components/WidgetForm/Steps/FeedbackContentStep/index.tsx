fadaa175106702cb98ce33e0f964bc1e6ab40961
Luciano Ferreira <luciano2023silva@gmail.com>
2022-05-08 01:08:45 -0300

    feat: Loading when submit feedback

--- src/components/WidgetForm/Steps/FeedbackContentStep/index.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep/index.tsx
@@ -3,6 +3,7 @@ import { FormEvent, useState } from 'react';
 import { FeedbackType, feedbackTypes } from '../../';
 import { api } from '../../../../libs/api';
 import { CloseButton } from '../../../CloseButton';
+import { Loading } from '../../../Loading';
 import { ScreenShotButton } from './ScreenShotButton';
 
 interface FeedbackContentStepProps {
@@ -18,11 +19,13 @@ export function FeedbackContentStep({
 }: FeedbackContentStepProps) {
   const [screenshot, setScreenshot] = useState<string | null>(null);
   const [comment, setComment] = useState('');
+  const [isSendingFeedback, setIsSendingFeedback] = useState(false);
 
   const feedbackTypeInfo = feedbackTypes[feedbackType];
 
   async function handleSubmitFeedback(e: FormEvent) {
     e.preventDefault();
+    setIsSendingFeedback(true)
     
     await api.post('/feedbacks', {
       type: feedbackType,
@@ -30,7 +33,10 @@ export function FeedbackContentStep({
       screenshot
     });
 
+    setIsSendingFeedback(false);
+    
     onFeedbackSent();
+
   }
   return (
     <>
@@ -71,13 +77,13 @@ export function FeedbackContentStep({
 
           <button
             type='submit'
-            disabled={comment.length === 0}
+            disabled={comment.length === 0 || isSendingFeedback}
             className='p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center 
           items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 
           focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 
           disabled:hover:bg-brand-500 disabled:cursor-not-allowed'
           >
-            Submit feedback
+            {isSendingFeedback ? <Loading /> : 'Submit feedback'}
           </button>
         </footer>
       </form>
